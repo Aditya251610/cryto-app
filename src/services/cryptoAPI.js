c3332513ad6e47582e449fe1b7d200e03e8749fa
Aditya251610 <aditya.sharma16062016@gmail.com>
2024-06-20 07:20:17 +0530

    crypto currency tracker app

---
+++ src/services/cryptoAPI.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-key': 'b8ab5a279fmshf1b1c55191d8144p15850ajsn879ff21eb5c9',
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
